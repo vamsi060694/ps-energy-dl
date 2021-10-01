edea73ea08c02d465ed2c3b763a9172ff64243f7
siri sagi <siri.sagi@itversity.in>
2021-10-01 18:13:41 +0530

    Code for Downloading the data files from url

--- Downloading the data files.py
+++ Downloading the data files.py
@@ -1,7 +1,15 @@
+from os.path import basename
 import requests
 
-file_url = 'https://www.cnlopb.ca/wp-content/uploads/hebstats/heb_oil_2017.pdf'
+urls = [
+    'https://www.cnlopb.ca/wp-content/uploads/hebstats/heb_oil_2017.pdf',
+    'https://www.cnlopb.ca/wp-content/uploads/hebstats/heb_oil_2018.pdf',
+    'https://www.cnlopb.ca/wp-content/uploads/hebstats/heb_oil_2019.pdf',
+    'https://www.cnlopb.ca/wp-content/uploads/hebstats/heb_oil_2020.pdf',
+    'https://www.cnlopb.ca/wp-content/uploads/hebstats/heb_oil_2021.pdf'
+    ]
 
-req = requests.get(file_url)
-with open('Heborn_well_2017.pdf', 'wb') as file:
-    file.write(req.content)
+for url in urls:
+    req = requests.get(url)
+    with open(basename(url), 'wb') as file:
+        file.write(req.content)
