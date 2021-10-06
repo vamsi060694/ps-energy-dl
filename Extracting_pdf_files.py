b27d8aad74ecb5f3eae16b2e5fc0c0e7691e3e3d
vamsi060694 <vamsi1994@outlook.com>
2021-10-07 00:27:32 +0530

    fill null values & empty spaces

--- Extracting_pdf_files.py
+++ Extracting_pdf_files.py
@@ -1,24 +1,32 @@
 import camelot
 import os
+import pandas as pd
 
 current_dir = os.getcwd()
-#for site in websites:
-    #website_year = site.split('/')[-1][-8:-4]
 
 
-def extract_tables(field_dir,year):
-    field_path = os.path.join(current_dir,field_dir)
-    for i in open(field_path,'r'):
-        if year == i.split('/')[-1][-8:-4]:
-            file_by_year = os.path.join(field_path, i)
-            pdf = open(file_by_year, 'r')
-            table = camelot.read_pdf(pdf, pages='all', flavour='stream')
-            all_tables = table.export(f'{pdf}.csv')
-            return all_tables
+# for site in websites:
+# website_year = site.split('/')[-1][-8:-4]
 
-extract_tables('heb','2017')
 
-n_path = 'hib_oil_2021.pdf-page-1-table-1.csv'
+def extract_tables(field_dir):
+    field_dir_path = os.path.join(current_dir, field_dir)
+    for year in open(field_dir_path, 'r'):
+        file_by_year = os.path.join(field_dir_path, year)
+        pdf = open(file_by_year, 'r')
+        tables = camelot.read_pdf(pdf, pages='all', flavour='stream')
+        all_tables = tables.export(f'{pdf}.csv')
+        return all_tables
 
-data = pd.read_csv(n_path,delimiter=',',header='infer')
-data.fillna({'Well Name':'HIB-B-016-001','Year':2021,'Month':0}).dropna()
+
+extract_tables('hebron')
+
+d_path = 'hib_oil_2021.pdf-page-1-table-1.csv'
+
+
+def reading_to_df(field_dir):
+    field_dir_path = os.path.join(current_dir, field_dir)
+
+    data = pd.read_csv(d_path, delimiter=',', header='infer')
+    data.fillna({'Well Name': 'HIB-B-016-001', 'Year': 2021, 'Month': 0}).dropna(). \
+        rename(columns={'Well Name': 'Well_names'})
