0b46171b9cd05bd1ce4407ce97a892c2fd118c33
vamsi060694 <vamsi1994@outlook.com>
2021-10-08 15:33:57 +0530

    reading pdf files

--- Cleaning_the_data.py
+++ Cleaning_the_data.py
@@ -1,10 +1,13 @@
 import os
-
+import glob
+import pandas as pd
 
 cwd = os.getcwd()
-print(cwd)
-path = "C:\workspace\ps-energy-dl\hebron"
+path = '/HIBERNIA'
 os.chdir(path)
+file_extension = '.csv'
+all_filenames = list([i for i in glob.glob(f'*{file_extension}')])
+print(all_filenames)
 
-
-
+df = [pd.read_csv(file, delimiter='\t',encoding='UTF-16') for file in all_filenames]
+camel
