31747296f520498b68edb8b5faaa40f89dfe799c
vamsi060694 <vamsi1994@outlook.com>
2021-10-09 23:40:16 +0530

    cleaning null values and spaces

--- Cleaning_the_data.py
+++ Cleaning_the_data.py
@@ -9,6 +9,15 @@ os.chdir(path)
 file_extension = '.csv'
 all_filenames = list([i for i in glob.glob(f'*{file_extension}')])
 print(all_filenames)
+df = pd.concat([pd.read_csv(file, delimiter=',') for file in all_filenames])
+df.columns = df.columns.str.replace(' ','_')
+print(df[:25])
+df= df[df.Well_Name != 'Yearly Total:']
+print(df)
+df = df.fillna(method='ffill')
+#print(df[:50])
+for columns in ['Well_Name', 'Year', 'Month']:
+    df[columns] = df[columns].ffill()
+print(df[:50])
 
-df = [pd.read_csv(file, delimiter=',') for file in all_filenames]
-print(df[1:])
+#data_new.drop('Total', axis=1).dropna()
