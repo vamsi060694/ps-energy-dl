a928d16dcfbbb8776c6d47bc257331f9646f1252
vamsi060694 <vamsi1994@outlook.com>
2021-10-14 11:20:06 +0530

    created a data directory for all fields

--- app/etl/Extracting_pdf_files.py
+++ app/etl/Extracting_pdf_files.py
@@ -1,11 +1,11 @@
 import os
-#import camelot
+
+import camelot
 directories = ['C:\workspace\ps-energy-dl\hebron_pdf',
                'C:\workspace\ps-energy-dl\hibernia_pdf',
                'C:\workspace\ps-energy-dl\methyst_north_pdf',
                'C:\workspace\ps-energy-dl\ova_terra_pdf',
                'C:\workspace\ps-energy-dl\white_ross_pdf']
-
 
 # def read_pdf(dir_path):
 #     field_dir_path = os.path.join(current_dir, dir_path)
@@ -25,16 +25,16 @@ directories = ['C:\workspace\ps-energy-dl\hebron_pdf',
 #     print(dir_path)
 #     read_pdf(dir_path)
 
+
 current_dir = os.getcwd()
 print(current_dir)
-
 
 for directory in directories:
     field_dir_path = os.path.join(current_dir, directory)
     for path, dirs, files in os.walk(field_dir_path):
         for f in files:
             print(f)
-            # filename = os.path.join(path, f)
-            # print(filename)
-            # tables = camelot.read_pdf(filename, pages='all', flavor='stream', edge_tol=1000)
-            # tables.export(f'{filename}.csv')
+            filename = os.path.join(path, f)
+            print(filename)
+            tables = camelot.read_pdf(filename, pages='all', flavor='stream', edge_tol=1000)
+            tables.export(f'{filename}.csv')
