27b6a1d54a3f7747fe60464a4ec83e55519d2ec9
siri sagi <siri.sagi@itversity.in>
2021-10-16 10:21:01 +0530

    Code for Downloading the required pdf data files from a given url

--- Downloading_the_data_files.py
+++ Downloading_the_data_files.py
@@ -3,10 +3,35 @@ from os.path import basename
 from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
+import datetime as dt
+import json
 
 load_dotenv()
 
 filters = os.getenv('REFERENCE').split(',')
+
+
+def all_years(data_path, field):
+    requirement = str(input('Do you want to download all years:'))
+    yes = os.getenv('ALL_YEARS')
+
+    if requirement == yes:
+        for source in sources:
+            req = requests.get(f'{url}/{source}')
+            with open(basename(f'{data_path}\\{field}\\{source}'), 'wb') as file:
+                file.write(req.content)
+
+    else:
+
+        d = dt.date.today()
+
+        for source in sources:
+            if source.split('.')[-2][-4:] == str(d.year):
+                req = requests.get(f'{url}/{source}')
+                with open(basename(f'{data_path}\\{field}\\{source}'), 'wb') as file:
+                    file.write(req.content)
+
+
 for filter in filters:
 
     url = f'https://www.cnlopb.ca/wp-content/uploads/{filter}'
@@ -19,12 +44,20 @@ for filter in filters:
         if 'pdf' in link['href']:
             sources.append(link.get('href'))
 
-    fields = os.environ["FOLDERS"].split(',')
-    data_path = os.getenv('FOLDER_PATH')
-    for field in fields:
-        os.mkdir(f'{data_path}\\{field}')
+    print(sources)
 
-    for source in sources:
-        req = requests.get(f'{url}/{source}')
-        with open(basename(r'{data_path}\\{field}\\{source}'), 'wb') as file:
-            file.write(req.content)
+    data_path = os.getenv('FOLDER_PATH')
+    fields = os.getenv('FOLDERS').split(',')
+    print(fields)
+
+    field = json.loads(os.getenv('FIELDS'))[filter]
+    # field = fields[filter]
+    print("===>", field)
+    os.chdir(data_path)
+    if not os.path.isdir(data_path + '\\' + field):
+        os.mkdir(field)
+    os.chdir(data_path + '\\' + field)
+    print(">>", os.getcwd())
+
+    all_years(data_path, field)
+    os.chdir(data_path)
