ebca31b20e999881e17e870a3b9139e078bbbba5
vamsi060694 <vamsi1994@outlook.com>
2021-10-18 20:29:47 +0530

    extracting based on columns

--- App/ETL/testing_extracting.py
+++ App/ETL/testing_extracting.py
@@ -1,30 +1,46 @@
+import os
 import camelot
+import pandas as pd
+path = 'C:\workspace\ps-energy-dl\data_folder\hibernia'
+for path, dirs, files in os.walk(path ):
+    for f in files:
+        #print(f)
+        filename = os.path.join(path, f)
+        #print(filename)
+        tables = camelot.read_pdf(filename, pages='all', flavor='stream',edge_tol=1000)
+        num = tables.n
+        f_data = tables[0].df
+        d_len = len(f_data.columns)
+        index = f_data.index[f_data.iloc[:,0] == 'Well Name'][0]
+        f_data = f_data[index + 1:]
+        if d_len == 6:
+            f_data.columns = ['Well Name', 'Year', 'Month', 'Oil_(m³)', 'Gas_(10³m³)', 'Water_(m³)']
+        else:
+            f_data.columns = ['Well Name', 'Year', 'Month', 'Total', 'Oil_(m³)', 'Gas_(10³m³)', 'Water_(m³)']
 
-# import sqlite3
+            for i in range(1,num):
+                temp_df = tables[i].df
+                d_len = len(temp_df.columns)
+                if d_len == 6:
+                    temp_df.columns = ['Well Name','Year','Month','Oil_(m³)','Gas_(10³m³)','Water_(m³)']
+                else:
+                    temp_df.columns = ['Well Name', 'Year', 'Month', 'Total', 'Oil_(m³)', 'Gas_(10³m³)', 'Water_(m³)']
 
-# import pandas as pd
-
-
-current_dir = os.getcwd()
-
-
-# print(current_dir)
-
-
-def read_pdf(dir_path):
-    field_dir_path = os.path.join(current_dir, dir_path)
-    for path, dirs, files in os.walk(field_dir_path):
-        for f in files:
-            filename = os.path.join(path, f)
-            #print(filename)
-            tables = camelot.read_pdf(filename, pages='all', flavor='stream', edge_tol=1000)
-            tables.export(f'{filename}.csv')
-            return tables
+                f_data = pd.concat([f_data,temp_df])
+                print(f_data[:50])
 
 
-folder_path = ['hibernia_pdf']
 
-for i in folder_path:
-    dir_path = os.path.join(current_dir, i)
-    print(dir_path)
-    read_pdf(dir_path)
+
+
+
+
+
+
+        # all_tables = pd.DataFrame()
+        # for i in range(1,n):
+        #     temp_df = (tables[i].df)
+        #     all_tables = pd.concat([all_tables,temp_df])
+        # print(all_tables)
+        # # print(type(tables[0]))
+        # # # tables.export(f'{filename}.csv')
--- app/etl/Extracting_pdf_files.py
+++ app/etl/Extracting_pdf_files.py
@@ -1,40 +1,82 @@
+#import camelot
 import os
+import logging
+from dotenv import load_dotenv
 
-import camelot
-directories = ['C:\workspace\ps-energy-dl\hebron_pdf',
-               'C:\workspace\ps-energy-dl\hibernia_pdf',
-               'C:\workspace\ps-energy-dl\methyst_north_pdf',
-               'C:\workspace\ps-energy-dl\ova_terra_pdf',
-               'C:\workspace\ps-energy-dl\white_ross_pdf']
+logging.basicConfig(filename='extraction.log',level=logging.INFO,
+                    format='%(asctime)s %(message)s')
 
-# def read_pdf(dir_path):
-#     field_dir_path = os.path.join(current_dir, dir_path)
-#     for path, dirs, files in os.walk(field_dir_path):
-#         for f in files:
-#             filename = os.path.join(path, f)
-#             #print(filename)
-#             tables = camelot.read_pdf(filename, pages='all', flavor='stream', edge_tol=1000)
-#             tables.export(f'{filename}.csv')
-#             return tables
+
+load_dotenv()
+
+folder_path = os.getenv('FOLDER_PATH')
+print(folder_path)
+data_path = os.path.join(folder_path,'data_folder')
+print(data_path)
+
+directories = os.listdir(data_path)
+logging.info(directories)
+
+# def extract(directories, data_path):
+#     for directory in directories:
+#         field_dir_path = os.path.join(data_path, directory)
+#         print(field_dir_path)
+#         for path, dirs, files in os.walk(field_dir_path):
+#             for f in files:
+#                 print(f)
+#                 filename = os.path.join(path, f)
+#                 print(filename)
+#                 tables = camelot.read_pdf(filename, pages='all', flavor='stream')
+#                 tables.export(f'{filename}.csv')
 #
 #
-# folder_path = ['hibernia_pdf']
+# extract(directories, data_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# path = '/app/etl/testing/data_folder\hibernia'
+# os.chdir(path)
+# for path, dirs, files in os.walk(path):
+#     for f in files:
+#         print(f)
+#         filename = os.path.join(path, f)
+#         print(filename)
+#         tables = camelot.read_pdf(filename, pages='all', flavor='stream')
+#         tables.export(f'{filename}.csv')
+
+# def extract(directories, data_path):
+#     for directory in directories:
+#         field_dir_path = os.path.join(data_path, directory)
+#         print(field_dir_path)
+#         for path, dirs, files in os.walk(field_dir_path):
+#             for f in files:
+#                 print(f)
+#                 filename = os.path.join(path, f)
+#                 print(filename)
+#                 tables = camelot.read_pdf(filename, pages='all', flavor='stream')
+#                 tables.export(f'{filename}.csv')
 #
-# for i in folder_path:
-#     dir_path = os.path.join(current_dir, i)
-#     print(dir_path)
-#     read_pdf(dir_path)
-
-
-current_dir = os.getcwd()
-print(current_dir)
-
-for directory in directories:
-    field_dir_path = os.path.join(current_dir, directory)
-    for path, dirs, files in os.walk(field_dir_path):
-        for f in files:
-            print(f)
-            filename = os.path.join(path, f)
-            print(filename)
-            tables = camelot.read_pdf(filename, pages='all', flavor='stream', edge_tol=1000)
-            tables.export(f'{filename}.csv')
+#
+# extract(directories, data_path)
