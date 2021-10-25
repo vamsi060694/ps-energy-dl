974914c9102406373146e87615b22349c057038d
siri sagi <siri.sagi@itversity.in>
2021-10-25 15:26:19 +0530

    Connect to database and insert the data

--- app/etl/database_connectivity.py
+++ app/etl/database_connectivity.py
@@ -1,41 +1,48 @@
-import psycopg2
+import os
+import sqlalchemy
 from app.etl.Transforming_data import transforming_data
+from dotenv import load_dotenv
+import pandas as pd
 
-host = 'localhost'
-port = '5433'
-database = 'itversity_energy_db'
-user = 'itversity_energy_user'
-password = 'energy_password'
+load_dotenv()
+
+host = os.getenv('HOST')
+port = os.getenv('PORT')
+database = os.getenv('DATABASE')
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
 
 
-def get_pg_connection(host, port, database, user, password):
-    connection = None
-    try:
-        connection = psycopg2.connect(
-            host=host,
-            port=port,
-            database=database,
-            user=user,
-            password=password
-        )
-    except Exception as e:
-        raise (e)
+def db_connection(host, port, database, user, password):
+    en_connection = sqlalchemy.create_engine(f"postgresql://{user}:{password}@{host}:{port}/{database}")
+    return en_connection
 
-    return connection
+energy_connection = db_connection(host, port, database, user, password)
 
-energy_connection = get_pg_connection(host, port, database, user, password)
+energy_schema = [
+    "well_name",
+    "month",
+    "Comodity",
+    "Value",
+    "energy",
+    "units"
+]
+meta = sqlalchemy.MetaData()
 
-cursor = energy_connection.cursor()
-query = ("""
-    INSERT INTO users
-        (well_name, month, Comodity, Value, energy, units)
-    VALUES
-        (%s, %s, %s, %s, %s, %s)
-""")
-
-user = transforming_data(sample_path)
-cursor.execute(query, user)
-energy_connection.commit()
-
-cursor.close()
-energy_connection.close()
+energy_table = sqlalchemy.Table(
+   'table', meta,
+   sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+   sqlalchemy.Column('well_name', sqlalchemy.String),
+   sqlalchemy.Column('month', sqlalchemy.DATE),
+   sqlalchemy.Column('Comodity', sqlalchemy.String),
+   sqlalchemy.Column('Value', sqlalchemy.FLOAT),
+   sqlalchemy.Column('energy', sqlalchemy.String),
+   sqlalchemy.Column('units', sqlalchemy.String)
+)
+meta.create_all(energy_connection)
+units_data = pd.read_csv(r'C:\Users\siri sagi\PycharmProjects\ps-energy-dl\units.csv', header=0, sep=',')
+product_data = pd.read_csv(r'C:\Users\siri sagi\PycharmProjects\ps-energy-dl\eneryproduct.csv', header=0, sep=',')
+data = transforming_data(r'C:\Users\siri sagi\PycharmProjects\ps-energy-dl\Sample data file - Sheet1.csv', units_data, product_data)
+data.to_sql(energy_table, schema=energy_schema, con=energy_connection, index=False, if_exists='replace', method='multi')
+# energy_table = sqlalchemy.Table('energy_table', meta, autoload=True, autoload_with=energy_connection)
+print(data.columns.keys())
