0f2fa5b8a0b9a669aa1b68ecae351242d84a844a
siri sagi <siri.sagi@itversity.in>
2021-12-01 12:49:56 +0530

    inserting the data

--- app/etl/dbinsertion.py
+++ app/etl/dbinsertion.py
@@ -1,24 +1,12 @@
-import os
 import sqlalchemy as sqlalchemy
 from app.etl.transform_data import transforming_data
 from dotenv import load_dotenv
 import pandas as pd
+from app.utils.db_connectivity import db_connection
 
 load_dotenv()
 
-host = os.getenv('HOST')
-port = os.getenv('PORT')
-database = os.getenv('DATABASE')
-user = os.getenv('USER')
-password = os.getenv('PASSWORD')
-
-
-def db_connection(host, port, database, user, password):
-    en_connection = sqlalchemy.create_engine(f"postgresql://{user}:{password}@{host}:{port}/{database}")
-    return en_connection
-
-
-energy_connection = db_connection(host, port, database, user, password)
+energy_connection = db_connection()
 
 meta = sqlalchemy.MetaData()
 energy_table = sqlalchemy.Table(
@@ -35,7 +23,7 @@ meta.create_all(energy_connection)
 units_data = pd.read_csv(r'units.csv', header=0, sep=',')
 product_data = pd.read_csv(r'energy.csv', header=0, sep=',')
 
-db_data = transforming_data(r'csv_file.csv', product_data,
+db_data = transforming_data(r'C:\Users\siri sagi\PycharmProjects\ps-energy-dl\data_folder\csv_file.csv', product_data,
                             units_data)
 print(type(db_data))
 
