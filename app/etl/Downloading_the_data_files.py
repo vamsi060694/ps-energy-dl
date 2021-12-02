41bcfc033e5717ab8d193fe7ce74c821088d2f6f
siri sagi <siri.sagi@itversity.in>
2021-12-02 11:12:52 +0530

    downloading code updated

--- app/etl/Downloading_the_data_files.py
+++ app/etl/Downloading_the_data_files.py
@@ -5,87 +5,89 @@ import requests
 from bs4 import BeautifulSoup
 import datetime as dt
 import json
+import logging
 
 load_dotenv()
 
 filters = os.getenv('REFERENCE').split(',')
+folder_path = os.getenv('FOLDER_PATH')
+data_path = os.path.join(folder_path + '//' + 'data_folder')
 
 
-def all_years(data_path, field):
+def all_years(data_path, field, sources, url):
     answer = os.getenv('ALL_YEARS')
+
     if answer == 'yes':
         try:
             for source in sources:
                 req = requests.get(f'{url}/{source}')
-                with open(basename(f'{data_path}\\{field}\\{source}'), 'wb') as file:
+                with open(basename(f'{data_path}//{field}//{source}'), 'wb') as file:
                     file.write(req.content)
-        except:
-            print("Not able to download the required pdf files ")
+        except Exception as e:
+            logging.error(e)
+
     else:
         try:
             d = dt.date.today()
             for source in sources:
                 if source.split('.')[-2][-4:] == str(d.year):
                     req = requests.get(f'{url}/{source}')
-                    with open(basename(f'{data_path}\\{field}\\{source}'), 'wb') as file:
+                    with open(basename(f'{data_path}//{field}//{source}'), 'wb') as file:
                         file.write(req.content)
-        except:
-            print("Not able to download the current year pdf files")
+        except Exception as e:
+            logging.error(e)
 
 
-for filter in filters:
-
-    def pdf_links(url):
+def providing_path(filters, data_path):
+    for filter in filters:
         try:
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
+            url = f'https://www.cnlopb.ca/wp-content/uploads/{filter}'
 
-            websites = []
-            for link in soup.find_all('a'):
-                if 'pdf' in link['href']:
-                    websites.append(link.get('href'))
-            return websites
-        except:
-            print("Not able to extract the required pdf links from the given url")
+            def pdf_links(url):
+                try:
+                    response = requests.get(url)
+                    soup = BeautifulSoup(response.text, 'html.parser')
 
+                    websites = []
+                    for link in soup.find_all('a'):
+                        if 'pdf' in link['href']:
+                            websites.append(link.get('href'))
+                    return websites
+                except Exception as e:
+                    logging.error(e)
 
-    sources = pdf_links(f'https://www.cnlopb.ca/wp-content/uploads/{filter}')
-    print(sources)
+            sources = pdf_links(url)
+            print(sources)
 
-    folder_path = os.getenv('FOLDER_PATH')
-    data_path = os.path.join(folder_path, "../../data_folder")
+            def creating_folder(data_path):
+                try:
+                    if not os.path.isdir(data_path):
+                        os.mkdir('../../data_folder')
+                except Exception as e:
+                    logging.error(e)
 
+            creating_folder(data_path)
 
-    def creating_directory(data_path):
-        try:
-            if not os.path.isdir(data_path):
-                os.mkdir("../../data_folder")
-                os.chdir(data_path)
-
-        except:
-            print("Not able to create the directory")
-
-
-    creating_directory(data_path)
-
-    fields = os.getenv('FOLDERS').split(',')
-    field = json.loads(os.getenv('FIELDS'))[filter]
-    # field = fields[filter]
-    print("===>", field)
-
-    def creating_fields(data_path, field):
-        try:
+            field = json.loads(os.getenv('FIELDS'))[filter]
+            # field = fields[filter]
+            print("===>", field)
             os.chdir(data_path)
-            if not os.path.isdir(data_path + '\\' + field):
-                os.mkdir(field)
-            os.chdir(data_path + '\\' + field)
-            print(">>", os.getcwd())
-        except:
-            print("Not able to create the respected folders")
+
+            def creating_fields(data_path, field):
+                try:
+                    if not os.path.isdir(data_path + '//' + field):
+                        os.mkdir(field)
+                    os.chdir(data_path + '//' + field)
+                    print(">>", os.getcwd())
+                except Exception as e:
+                    logging.error(e)
+
+            creating_fields(data_path, field)
+            all_years(data_path, field, sources, url)
+            os.chdir(data_path)
+
+        except Exception as e:
+            logging.error(e)
 
 
-    creating_fields(data_path, field)
-
-    all_years(data_path, field)
-    os.chdir(data_path)
-
+providing_path(filters, data_path)
